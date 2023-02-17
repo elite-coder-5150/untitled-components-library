a3db4a000d26cd89a55de0957e79621a77764520
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-17 15:04:18 -0600

    defined the toggle drop down menu method

--- js/drop-down-menu.js
+++ js/drop-down-menu.js
@@ -2,9 +2,23 @@
 class DropDownMenu {
     init() {}
 
-    cacheDom() {}
+    cacheDom() {
+        this.dropDownMenu = document.querySelector('.drop-down-menu')
+    }
 
-    initEvents() {}
+    initEvents() {
+        this.dropDownMenu.addEventListener('click', this.toggleDropDown())
+    }
+
+    toggleDropDown() {
+        if (!(this.dropDownMenu.classList.contains('show'))) {
+            this.dropDownMenu.classList.add('show');
+            this.dropDownMenu.classList.remove('hidden');
+        } else if (this.dropDownMenu.classList.contains('hidden')) {
+            this.dropDownMenu.classList.remove('hidden');
+            this.dropDownMenu.classList.add('show');
+        }
+    }
 }
 
 /* eslint-env node */
