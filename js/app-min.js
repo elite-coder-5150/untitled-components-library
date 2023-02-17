b1b98c2dcf0729583877c3522d3df309c00bc0f8
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-17 15:03:48 -0600

    minified version

--- js/app-min.js
+++ js/app-min.js
@@ -1,1 +1,1 @@
-import{DropDownMenu}from"./drop-down-menu";class App{init(){this.droopdownMenu=DropDownMenu.init()}}let t=new App;t.init();
+import{DropDownMenu}from"./drop-down-menu";import{Notify}from"./notifications";class App{init(){this.droopdownMenu=DropDownMenu.init(),this.notify=Notify.init()}}let t=new App;t.init();
