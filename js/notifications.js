b1490067bf72651e1f6110259056c61be8b2db6d
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-02-17 15:03:26 -0600

    defined the toggle notification method

---
+++ js/notifications.js
@@ -0,0 +1,17 @@
+export class Notify {
+    init() {
+        this.cacheDom();
+        this.initEvents();
+    }
+
+    cacheDom() {
+
+    }
+
+    initEvents() {
+
+    }
+}
+
+let n = new Notify();
+n.init();
